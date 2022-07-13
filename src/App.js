89f4a29f03f137b5486621042f761e8c39731187
Sandeep Yadav <sandeepuim10@gmail.com>
2022-07-13 15:32:43 +0530

    LAZY-LOADING

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+// import About from './About'
+// import Friends from './Friends'
+const About = React.lazy(() => import("./About"));
+const Friends = React.lazy(() => import("./Friends"));
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <About />
+      <Friends />
     </div>
   );
-}
+};
 
 export default App;
